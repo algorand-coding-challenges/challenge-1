db996e01a8ba1885457cc8b7576343a5086dc0b8
dimzachar <zacharenakis@proton.me>
2024-03-05 20:11:51 +0200

    Update index.ts
    algo challenge 1

--- challenge/index.ts
+++ challenge/index.ts
@@ -29,7 +29,12 @@ const txn = algosdk.makePaymentTxnWithSuggestedParamsFromObject({
     amount: 1000000,
 });
 
-await algodClient.sendRawTransaction(txn).do();
+const signedTxn = txn.signTxn(sender.sk); // Sign the transaction with sender's private key
+
+// Submit the transaction
+await algodClient.sendRawTransaction(signedTxn).do();
+    
+
 const result = await algosdk.waitForConfirmation(
     algodClient,
     txn.txID().toString(),
