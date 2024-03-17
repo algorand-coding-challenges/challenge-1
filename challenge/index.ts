f704491e31e54d236bb25c9ac9dfdf42cc2fc737
ch-Abhinay <120096746+ch-Abhinay@users.noreply.github.com>
2024-03-17 12:55:10 +0530

    index.ts

--- challenge/index.ts
+++ challenge/index.ts
@@ -1,39 +1,48 @@
-import algosdk  from "algosdk";
+import algosdk from "algosdk";
 import * as algokit from '@algorandfoundation/algokit-utils';
 
-const algodClient = algokit.getAlgoClient()
+const algodClient = algokit.getAlgoClient();
 
 // Retrieve 2 accounts from localnet kmd
-const sender = await algokit.getLocalNetDispenserAccount(algodClient)
+const sender = await algokit.getLocalNetDispenserAccount(algodClient);
 const receiver = await algokit.mnemonicAccountFromEnvironment(
-    {name: 'RECEIVER', fundWith: algokit.algos(100)},
+    { name: 'RECEIVER', fundWith: algokit.algos(100) },
     algodClient,
-  )
+);
 
 /*
 TODO: edit code below
 
 Puzzle: 
 The below code is trying to send a payment from accounts[0] to accounts[1]. 
-However, the code is not working. fix the code so that the payment is sent 
+However, the code is not working. Fix the code so that the payment is sent 
 successfully. 
 
 When solved correctly, the console should print out the following:
 "Payment of 1000000 microAlgos was sent to RRYKB23LFR62G3P4SFINZDQ4FVDUNWWQ4NOF7K6TP5GO65BQCHYMNTR3CU at confirmed round 59"
 */
-const suggestedParams = await algodClient.getTransactionParams().do();
-const txn = algosdk.makePaymentTxnWithSuggestedParamsFromObject({
-    from: sender.addr,
-    suggestedParams,
-    to: receiver.addr,
-    amount: 1000000,
-});
 
-await algodClient.sendRawTransaction(txn).do();
-const result = await algosdk.waitForConfirmation(
-    algodClient,
-    txn.txID().toString(),
-    3
+// Retrieve suggested transaction parameters
+const suggestedParams = await algodClient.getTransactionParams().do();
+
+// Create payment transaction
+const txn = algosdk.makePaymentTxnWithSuggestedParams(
+    sender.addr, // Sender's address
+    receiver.addr, // Receiver's address
+    1000000, // Amount in microAlgos
+    undefined, // CloseRemainderTo: the address to close any remaining balance to
+    undefined, // Note: optional, any arbitrary data
+    suggestedParams // Suggested transaction parameters
 );
 
-console.log(`Payment of ${result.txn.txn.amt} microAlgos was sent to ${receiver.addr} at confirmed round ${result['confirmed-round']}`);
+// Sign the transaction
+const signedTxn = txn.signTxn(sender.sk);
+
+// Submit the transaction
+const txId = signedTxn.txID().toString();
+await algodClient.sendRawTransaction(signedTxn.blob).do();
+
+// Wait for confirmation
+const confirmedTxn = await algodClient.waitForConfirmation(txId, 3);
+
+console.log(`Payment of ${txn.amount} microAlgos was sent to ${receiver.addr} at confirmed round ${confirmedTxn['confirmed-round']}`);
